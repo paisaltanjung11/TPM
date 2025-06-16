e3850ac62efa3a46ec821c6151013c95114bac78
Paisal Tanjung <paisaltanjung1102@gmail.com>
2025-06-16 15:00:19 +0700

    test5

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Dashboard</title>
-  <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}" />
+  <link rel="stylesheet" href="{{ secure_asset('css/dashboard.css') }}">
 </head>
 
 <body>
