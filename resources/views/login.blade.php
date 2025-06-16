3d34e40dacae89a8b0b83f65973a553ba8c92a04
Paisal Tanjung <paisaltanjung1102@gmail.com>
2025-06-16 15:32:53 +0700

    test7

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login Page</title>
   <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
+
 <body>
-<audio id="Background-music" src="/audio/register login admin dashboard bg music.mp3" autoplay loop></audio>
+  <audio id="Background-music" src="/audio/register login admin dashboard bg music.mp3" autoplay loop></audio>
 
   <div class="background">
     <div class="login-container">
       <div class="first-line">
-        <img id="logo-techno" src="/Image/login image/logo techno.png" alt="Logo TechnoScape">
-        <img id="logo-hackathon" src="/Image/login image/logo hack.png" alt="Logo Hackathon"> 
+        <img id="logo-techno" src="{{ asset('image/logo_techno.png') }}" alt="Logo TechnoScape">
+        <img id="logo-hackathon" src="{{ asset('image/logo_hack.png') }}" alt="Logo Hackathon">
       </div>
       <div class="second-line">
         <h1>WELCOME!</h1>
@@ -23,14 +25,14 @@
       </div>
 
       @if ($errors->has('loginError'))
-    <div class="alert alert-danger">
-        {{ $errors->first('loginError') }}
-    </div>
-@endif
+      <div class="alert alert-danger">
+      {{ $errors->first('loginError') }}
+      </div>
+    @endif
 
       <div class="form-container">
         <form id="login-form" action="{{route('loginTeam')}}" method="post">
-            @csrf
+          @csrf
           <div>
             <input type="text" id="team-name" placeholder="Team name" name="name">
             <p class="error-message" id="error-teamName"></p>
@@ -47,10 +49,10 @@
           </div>
         </form>
       </div>
-      
+
       <div class="fourth-line">
         <p>Belum memiliki akun?</p>
-        <a href="Register">Registrasi disini  </a>
+        <a href="Register">Registrasi disini </a>
       </div>
 
       <img id="mascot" src="/Image/login image/Mascot .png" alt="">
@@ -58,4 +60,5 @@
   </div>
   <script src="{{ asset('js/login.js') }}"></script>
 </body>
+
 </html>
