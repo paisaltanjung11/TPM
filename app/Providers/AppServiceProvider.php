6f3dab02ae134021e97f7f9c2ffe15f319d7ca15
Paisal Tanjung <paisaltanjung1102@gmail.com>
2025-06-16 15:15:56 +0700

    test6

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,7 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
+        // Hanya paksa HTTPS di environment production
+        if (App::environment('production')) {
             URL::forceScheme('https');
         }
     }
